5f4671e5e08c1660ab648edd130313e819eb7b55
b-gran <bill@billgranfield.com>
2017-02-28 22:14:56 +0000

    Clean up build process + add static assets

--- utils/build.js
+++ utils/build.js
@@ -1,7 +1,21 @@
 var webpack = require("webpack"),
-    config = require("../webpack.config");
+  config = require("../webpack.config"),
+  fs = require('fs-extra'),
+  path = require('path');
 
 require("./prepare");
+
+// Copy static assets to the build directory
+fs.copy(
+  path.join(__dirname, '../src/static/'),
+  path.join(__dirname, '../build/static/'),
+  err => {
+    if (err) {
+      console.error('Failed to copy static assets');
+      console.log(err);
+    }
+  }
+);
 
 webpack(
   config,
--- utils/generate_manifest.js
+++ utils/generate_manifest.js
@@ -1,11 +1,12 @@
 var manifest = require("../src/manifest.json"),
-    fileSystem = require("fs"),
-    path = require("path"),
-    env = require("./env");
+  packageJson = require('../package.json'),
+  fileSystem = require("fs"),
+  path = require("path"),
+  env = require("./env");
 
 // generates the manifest file using the package.json informations
-manifest.description = process.env.npm_package_description;
-manifest.version = process.env.npm_package_version;
+manifest.description = packageJson.description;
+manifest.version = packageJson.version;
 
 fileSystem.writeFileSync(
   path.join(__dirname, "../build/manifest.json"),
