49a0c264565bbb2b7743e300f40f5844013eba2c
b-gran <bill@billgranfield.com>
2017-02-28 22:30:37 +0000

    Clean up build scripts

--- src/js/popup/greeting_component.jsx
+++ src/js/popup/greeting_component.jsx
@@ -2,6 +2,17 @@ import React from "react";
 
 export default class extends React.Component {
   render () {
-    return <p>Hello, find me on src/js/popup/greeting_component.jsx</p>;
+    return <p>
+      Hello, find me on src/js/popup/greeting_component.jsx
+
+      <p>
+        This is another paragraph! let's see how well the build
+        process is working.
+      </p>
+
+      <p>
+        Here's another paragraph still: let's see if it's still working.
+      </p>
+    </p>;
   }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ var webpack = require("webpack"),
     path = require("path"),
     fileSystem = require("fs"),
     env = require("./utils/env"),
+    _ = require('lodash'),
+    path = require('path'),
     HtmlWebpackPlugin = require("html-webpack-plugin"),
     WriteFilePlugin = require("write-file-webpack-plugin");
 
@@ -14,46 +16,64 @@ if (fileSystem.existsSync(secretsPath)) {
   alias["secrets"] = secretsPath;
 }
 
+const Entries = {
+  popup: Entry(
+    'popup',
+    path.join(__dirname, "src", "js", "popup.js"),
+    path.join(__dirname, "src", "popup.html")
+  ),
+
+  // options: Entry(
+  //   'options',
+  //   path.join(__dirname, "src", "js", "options.js"),
+  //   path.join(__dirname, "src", "options.html")
+  // ),
+  //
+  // background: Entry(
+  //   'background',
+  //   path.join(__dirname, "src", "js", "background.js"),
+  //   path.join(__dirname, "src", "background.html")
+  // ),
+};
+
+function Entry (id, filename, htmlFile) {
+  return {
+    id: id,
+    filename: filename,
+    htmlFile: htmlFile
+  };
+}
+
 module.exports = {
-  entry: {
-    popup: path.join(__dirname, "src", "js", "popup.js"),
-    options: path.join(__dirname, "src", "js", "options.js"),
-    background: path.join(__dirname, "src", "js", "background.js")
-  },
+  entry: _.mapValues(Entries, entry => entry.filename),
+
   output: {
     path: path.join(__dirname, "build"),
     filename: "[name].bundle.js"
   },
+
   module: {
     rules: [
       { test: /\.(js|jsx)$/, loader: "babel-loader" },
       { test: /\.css$/, loader: "style-loader!css-loader" }
     ]
   },
+
   resolve: {
     alias: alias,
     extensions: [".js", ".jsx", ".css"]
   },
+
   plugins: [
     // expose and write the allowed env vars on the compiled bundle
     new webpack.DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify(env.NODE_ENV)
     }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "popup.html"),
-      filename: "popup.html",
-      chunks: ["popup"]
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "options.html"),
-      filename: "options.html",
-      chunks: ["options"]
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "background.html"),
-      filename: "background.html",
-      chunks: ["background"]
-    }),
-    new WriteFilePlugin()
-  ]
+  ].concat(_.values(Entries).map(entry => new HtmlWebpackPlugin({
+      template: entry.htmlFile,
+      filename: path.basename(entry.htmlFile),
+      chunks: [entry.id]
+    })
+  )).concat([ new WriteFilePlugin() ]),
+
 };
