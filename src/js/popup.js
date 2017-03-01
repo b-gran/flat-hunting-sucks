8eeb502a4a4096a87f7d43bd4559c6ea833d5fde
b-gran <bill@billgranfield.com>
2017-03-01 20:24:54 +0000

    Stores form state in local storage

--- src/js/popup.js
+++ src/js/popup.js
@@ -7,12 +7,56 @@ import 'whatwg-fetch'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import reducer from './reducers/index'
+import _ from 'lodash'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-const store = createStore(reducer)
+function getFacade (_store) {
+  return {
+    subscribe (onChange, select = _.identity) {
+      let lastState = select(_store.getState())
+
+      return _store.subscribe(() => {
+        const currentState = select(_store.getState())
+
+        if (lastState !== currentState) {
+          lastState = currentState
+          return onChange(currentState)
+        }
+      })
+    }
+  }
+}
+
+function restoreFromStorage() {
+  try {
+    const form = JSON.parse(window.localStorage.getItem('form'))
+    if (_.isObject(form) && form !== null) {
+      return {
+        form: form
+      }
+    }
+  } catch (err) {}
+
+  return {}
+}
+
+const store = createStore(reducer, restoreFromStorage())
+
+const facade = getFacade(store)
+facade.subscribe(
+  form => {
+    const nonEmptyFields = _.pickBy(
+      form,
+      value => value && _.trim(value).length > 0
+    )
+
+    window.localStorage.setItem('form', JSON.stringify(nonEmptyFields))
+  },
+  _.property('form')
+)
 
 ReactDOM.render(
   <Provider store={store}>
